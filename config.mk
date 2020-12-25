aa2a23a37a7f0c9ad8c58fff70fd3268d67fcce6
Krmttt <74637232+Krmttt@users.noreply.github.com>
2020-12-25 20:00:21 +0800

    Update config.mk

--- config.mk
+++ config.mk
@@ -4,30 +4,31 @@ PLUGIN_MODULES := \
 	packet_plugin_rjv3
 
 # Linux
-PLUGIN_MODULES += if_impl_sockraw
+#PLUGIN_MODULES += if_impl_sockraw
 
 # macOS / BSD
 # PLUGIN_MODULES += if_impl_bpf
 
 # Other OS
-# PLUGIN_MODULES += if_impl_libpcap
+ PLUGIN_MODULES += if_impl_libpcap
 
-# This implementation is not included in the code.
-# If you need it, add it yourselves.
-# PLUGIN_MODULES += ifaddrs
+ This implementation is not included in the code.
+ If you need it, add it yourselves.
+ PLUGIN_MODULES += ifaddrs
 
 ENABLE_DEBUG  := false
-ENABLE_ICONV  := true
-ENABLE_GBCONV := false
-STATIC_BUILD  := false
+ENABLE_ICONV  := false
+ENABLE_GBCONV := true
+#STATIC_BUILD  := false
 
 # If your platform has iconv_* integrated into libc, change to false
 # Affects dynamic linking
 LIBICONV_STANDALONE := false
 
-CUSTOM_CFLAGS :=
-CUSTOM_LDFLAGS :=
-CUSTOM_LIBS :=
+CUSTOM_CFLAGS := 
+CUSTOM_LDFLAGS += -L/~/libpcap-1.9.1
+CUSTOM_LIBS += ~/libpcap-1.9.1/libpcap.a
+CC := mipsel-openwrt-linux-gcc
 
 # Example for cross-compiling
 # CC := arm-brcm-linux-uclibcgnueabi-gcc
